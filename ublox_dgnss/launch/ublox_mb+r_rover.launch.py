081ab51ae4b2cfbfc2af1a787733d343e2a33928
iman01 <iman.esfandiyar71@gmail.com>
2024-04-01 12:54:05 +0200

    added ublox configuration files,

--- ublox_dgnss/launch/ublox_mb+r_rover.launch.py
+++ ublox_dgnss/launch/ublox_mb+r_rover.launch.py
@@ -11,19 +11,19 @@ def generate_launch_description():
 
             # config measurement interval to 200 ms (ie 5 Hz) and nav update rate to once per measurement
             # {'CFG_RATE_MEAS': 0x3e8}, #1000ms
-            {'CFG_RATE_MEAS': 0xc8}, # 200ms
+            {'CFG_RATE_MEAS': 200}, # 200ms
             {'CFG_RATE_NAV': 0x1},
 
-            # # disable all messages on UART1
-            # {'CFG_UART1INPROT_NMEA': False},
-            # {'CFG_UART1INPROT_RTCM3X': False},
-            # {'CFG_UART1INPROT_UBX': False},
-            # {'CFG_UART1OUTPROT_NMEA': False},
-            # {'CFG_UART1OUTPROT_RTCM3X': False},
-            # {'CFG_UART1OUTPROT_UBX': False},
+            # disable all messages on UART1
+            {'CFG_UART1INPROT_NMEA': False},
+            {'CFG_UART1INPROT_RTCM3X': False},
+            {'CFG_UART1INPROT_UBX': False},
+            {'CFG_UART1OUTPROT_NMEA': False},
+            {'CFG_UART1OUTPROT_RTCM3X': False},
+            {'CFG_UART1OUTPROT_UBX': False},
 
             # set UART2 baud rate to 460800
-            {'CFG-UART2-BAUDRATE': 0x70800},
+            # {'CFG-UART2-BAUDRATE': 0x70800},
 
             # # receive RTCM messages only (from base) on UART2
             # {'CFG_UART2INPROT_NMEA': False},
